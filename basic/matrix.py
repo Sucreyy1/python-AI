1670daeedff96666513bc125334786b918b41302
Sucreyy1 <593143487@qq.com>
2018-01-15 15:06:39 +0800

    init

---
+++ basic/matrix.py
@@ -0,0 +1,8 @@
+import numpy as np
+import numpy.linalg as lg
+
+np_arrayA = np.array([[1, 2],
+                      [0, 3]], dtype=np.int32)
+
+np_arrayB = np.array([[2, 6],
+                      [3, 5]], dtype=np.int32)
